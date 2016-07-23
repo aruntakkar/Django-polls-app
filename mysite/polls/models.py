9c61b9ce3830c395ba7df607284df2e342c06239
aruntakkar <aruntakkar5@gmail.com>
2016-07-23 18:35:48 +0530

    django tutorial 2 Done

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,6 +13,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,11 +24,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-# Adding a Custom method for demonstration
-
-
-class Question(models.Model):
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
